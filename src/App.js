9f243bb710bb0ef838a91d25fa06bfe7b6fad5c9
roniardynt <dev@ron.my.id>
2020-10-14 11:04:27 +0700

    fix font size

--- src/App.js
+++ src/App.js
@@ -81,47 +81,47 @@ function Child() {
     <div tw="w-full flex-row max-w-xl bg-white shadow-lg overflow-hidden border-gray-700 border-4">
       <div
         className="header"
-        tw="font-bold text-sm md:text-xl py-2 px-2 border-gray-400 border-b-4 bg-gray-700 text-white mt-1 mr-1 ml-1"
+        tw="font-bold text-base md:text-xl py-2 px-2 border-gray-400 border-b-4 bg-gray-700 text-white mt-1 mr-1 ml-1"
       >
         <span>{kotaku}</span>
       </div>
       {status ? (
         <div className="jadwal" tw="flex flex-row justify-center text-center">
           <div className="subuh" tw="m-3">
-            <p tw="font-bold text-sm md:text-xl lg:text-2xl text-gray-700">
+            <p tw="font-bold text-base md:text-xl lg:text-2xl text-gray-700">
               {dataShalat.subuh}
             </p>
-            <p tw="font-bold text-sm md:text-xl text-red-600">Subuh</p>
+            <p tw="font-bold text-base md:text-xl text-red-600">Subuh</p>
           </div>
           <div className="dhuha" tw="m-3">
-            <p tw="font-bold text-sm md:text-xl lg:text-2xl text-gray-700">
+            <p tw="font-bold text-base md:text-xl lg:text-2xl text-gray-700">
               {dataShalat.dhuha}
             </p>
-            <p tw="font-bold text-sm md:text-xl text-red-600">Dhuha</p>
+            <p tw="font-bold text-base md:text-xl text-red-600">Dhuha</p>
           </div>
           <div className="dzuhur" tw="m-3">
-            <p tw="font-bold text-sm md:text-xl lg:text-2xl text-gray-700">
+            <p tw="font-bold text-base md:text-xl lg:text-2xl text-gray-700">
               {dataShalat.dzuhur}
             </p>
-            <p tw="font-bold text-sm md:text-xl text-red-600">Dzuhur</p>
+            <p tw="font-bold text-base md:text-xl text-red-600">Dzuhur</p>
           </div>
           <div className="asar" tw="m-3">
-            <p tw="font-bold text-sm md:text-xl lg:text-2xl text-gray-700">
+            <p tw="font-bold text-base md:text-xl lg:text-2xl text-gray-700">
               {dataShalat.ashar}
             </p>
-            <p tw="font-bold text-sm md:text-xl text-red-600">Ashar</p>
+            <p tw="font-bold text-base md:text-xl text-red-600">Ashar</p>
           </div>
           <div className="maghrib" tw="m-3">
-            <p tw="font-bold text-sm md:text-xl lg:text-2xl text-gray-700">
+            <p tw="font-bold text-base md:text-xl lg:text-2xl text-gray-700">
               {dataShalat.maghrib}
             </p>
-            <p tw="font-bold text-sm md:text-xl text-red-600">Maghrib</p>
+            <p tw="font-bold text-base md:text-xl text-red-600">Maghrib</p>
           </div>
           <div className="isya" tw="m-3">
-            <p tw="font-bold text-sm md:text-xl lg:text-2xl text-gray-700">
+            <p tw="font-bold text-base md:text-xl lg:text-2xl text-gray-700">
               {dataShalat.isya}
             </p>
-            <p tw="font-bold text-sm md:text-xl text-red-600">Isya'</p>
+            <p tw="font-bold text-base md:text-xl text-red-600">Isya'</p>
           </div>
         </div>
       ) : (
